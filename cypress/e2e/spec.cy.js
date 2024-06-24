def23b10cb173cb189c427086eb0d5536f0fb717
Vinicius Faria <artixsproductions@gmail.com>
2024-06-24 11:45:42 -0300

    feat: added 3 new tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,57 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Limpa tarefas completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+    
+    cy.get('.todo-list li .toggle')
+    .first()
+    .click();
+
+    cy.contains('Clear completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1);
+    
+  });
+
+  it('Tenta editar um todo', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Faxina{Enter}');
+    
+    cy.get('.todo-list li')
+      .first()
+      .dblclick()
+      .type(" de casa{Enter}");
+    
+    cy.get('.todo-list li')
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'Faxina de casa'); 
+  });
+
+  it('Verifica contador de tarefas', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+    
+    cy.get('.todo-count')
+      .should('have.text', '2 items left')
+
+    cy.get('.todo-list li .destroy')
+      .first()
+      .invoke('show')
+      .click();
+
+    cy.get('.todo-count')
+    .should('have.text', '1 item left')
+  })
 });
